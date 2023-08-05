65dd617fe06dbea39178b72e6834cf2f41aa3040
Esteban Brito <estebanbritop@gmail.com>
2023-08-05 11:44:17 -0500

    Models and stored procedures

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    Booking_date = models.DateField()
+    
+class Menu(models.Model):
+    ID = models.AutoField(primary_key=True)
+    titte = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
