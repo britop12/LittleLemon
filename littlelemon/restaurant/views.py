b802087d859189f3a84d193cb3d494cd740b95ae
Esteban Brito <estebanbritop@gmail.com>
2023-08-07 10:17:18 -0500

    User Authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,12 +3,16 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, Re
 from rest_framework.viewsets import ModelViewSet 
 from  .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+
+permission_classes = [IsAuthenticated]
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
